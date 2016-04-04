d1dea2dd5008c1088fd321fdb380353d5e45ce79
Mr9esx <1138099359@qq.com>
2016-04-04 23:35:35 +0800

    alpha v1.0.7

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 require_once ('./Module/func.php');
+define('IN_SYS', TRUE);
 if(file_exists("./config.php")){
 	//检测是否已安装。（简单检测）
 	require_once ('./config.php');
@@ -33,7 +34,16 @@ if(file_exists("./config.php")){
 			header('HTTP/1.1 404 Not Found'); 
 			header('location: /404.html');	
 		}
-	?>
+	?><div align="center" id="footer" class="container">
+			<div class="col-md-12 column" style="margin-top:60px;margin-bottom:30px;">
+				<div style="text-align:center;font-size:0.9em;color:#777;">
+					Copyright © 2016 · All Rights Reserved · Mr9esx from NERVGEEK
+				</div>
+				<div style="text-align:center;font-size:0.9em;color:#777;">
+				欢迎，这里是匿名版，以宅文化为主题的贴图论坛，在这里不需要注册即可发言。</br>平等的身份，中立的环境，让你尽情讨论你所感兴趣的话题。 
+				</div>
+			</div>
+		</div>
 		</div> 
 	</body>
 </html>
